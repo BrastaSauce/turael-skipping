c2f33b40443f97aa04055405f34afd1d9b463cb0
BrastaSauce <12422316+BrastaSauce@users.noreply.github.com>
2025-09-08 16:54:41 -0400

    Remove leftover wildcard import

--- src/main/java/com/brastasauce/turaelskipping/TuraelSkippingConfig.java
+++ src/main/java/com/brastasauce/turaelskipping/TuraelSkippingConfig.java
@@ -29,7 +29,7 @@ import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 import net.runelite.client.config.ConfigSection;
 
-import java.awt.*;
+import java.awt.Color;
 
 @ConfigGroup(TuraelSkippingConfig.CONFIG_GROUP_NAME)
 public interface TuraelSkippingConfig extends Config {
